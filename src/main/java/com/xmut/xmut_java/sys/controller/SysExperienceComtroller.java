5724322fc1d1f10a5a0ed89391f0e7fa4d33155c
1900 <747671402@qq.com>
2020-02-17 22:26:40 +0800

    论坛功能中的文章的增删改以及收藏功能的完善

--- src/main/java/com/xmut/xmut_java/sys/controller/SysExperienceComtroller.java
+++ src/main/java/com/xmut/xmut_java/sys/controller/SysExperienceComtroller.java
@@ -1,7 +1,9 @@
 package com.xmut.xmut_java.sys.controller;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xmut.xmut_java.common.BaseController;
 import com.xmut.xmut_java.common.Result;
@@ -149,6 +152,83 @@ public class SysExperienceComtroller extends BaseController{
 				result.setMessage("提交成功,请前往查看");
 			}
 		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	@RequestMapping("/deleteExperience")
+	public Result deleteExperience(Long id, HttpServletRequest request) {
+		Result result = new Result();
+		
+		try {
+			SysUser currentUser = (SysUser)request.getSession().getAttribute("user");
+			Long userId = currentUser.getId();
+			Map<String, Object> columnMap = new HashMap<String, Object>();
+			Map<String, Object> exColumnMap = new HashMap<String, Object>();
+			columnMap.put("user_id", userId);
+			columnMap.put("experience_id", id);
+			
+			sysUserExperienceMapper.deleteByMap(columnMap);
+			sysFavorExperienceMapper.deleteByMap(columnMap);
+			
+			exColumnMap.put("id", id);
+			
+			sysExperienceMapper.deleteByMap(exColumnMap);
+			
+			result.setMessage("删除成功，点击确定前往查看!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	@RequestMapping("/modifyExperience")
+	public Result modifyExperience(String title, String content, Long id, HttpServletRequest request) {
+		Result result = new Result();
+		
+		try {
+			SysExperience params = new SysExperience();
+			
+			params.setId(id);
+			SysExperience oldExperience = sysExperienceMapper.selectOne(new QueryWrapper<SysExperience>(params));
+			
+			if (oldExperience != null) {
+				oldExperience.setTitle(title);
+				oldExperience.setContent(content);
+				oldExperience.setModifyTime(new Date());
+				sysExperienceMapper.update(oldExperience, new UpdateWrapper<SysExperience>().eq("id", id));
+				result.setMessage("修改成功，点击确定前往查看!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	@RequestMapping("/isMyExperience")
+	public Result isMyExperience(Long id, HttpServletRequest request) {
+		Result result = new Result();
+		
+		try {
+			SysUser currentUser = (SysUser)request.getSession().getAttribute("user");
+			Long userId = currentUser.getId();
+			SysUserExperience params = new SysUserExperience();
+			
+			params.setUserId(userId);
+			params.setExperienceId(id);
+			
+			SysUserExperience isMy = sysUserExperienceMapper.selectOne(new QueryWrapper<SysUserExperience>(params));
+			
+			if (isMy != null) {
+				result.success("是我的");
+			} else {
+				result.fail("不是我的");
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
--- src/main/java/com/xmut/xmut_java/sys/controller/SysFavorController.java
+++ src/main/java/com/xmut/xmut_java/sys/controller/SysFavorController.java
@@ -12,15 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.xmut.xmut_java.common.BaseController;
 import com.xmut.xmut_java.common.Result;
+import com.xmut.xmut_java.sys.entity.SysExperience;
 import com.xmut.xmut_java.sys.entity.SysFavorExperience;
 import com.xmut.xmut_java.sys.entity.SysUser;
+import com.xmut.xmut_java.sys.mapper.SysExperienceMapper;
 import com.xmut.xmut_java.sys.mapper.SysFavorExperienceMapper;
 
 @RestController
 @RequestMapping("/collect")
 public class SysFavorController extends BaseController{
+	@Autowired
+	private SysExperienceMapper sysExperienceMapper;
+	
 	@Autowired
 	private SysFavorExperienceMapper sysFavorExperienceMapper;
 	
@@ -61,8 +67,16 @@ public class SysFavorController extends BaseController{
 			
 			SysFavorExperience isCollect = sysFavorExperienceMapper.selectOne(new QueryWrapper<SysFavorExperience>(params));
 			
-			params.setModifyTime(new Date());
-			if (isCollect == null) sysFavorExperienceMapper.insert(params);
+			if (isCollect == null)
+			{
+				SysExperience exParams = new SysExperience();
+				exParams.setId(id);
+				SysExperience experience = sysExperienceMapper.selectOne(new QueryWrapper<SysExperience>(exParams));
+				experience.setFavorNum(experience.getFavorNum() + 1);
+				sysExperienceMapper.update(experience, new UpdateWrapper<SysExperience>().eq("id", id));
+				params.setModifyTime(new Date());
+				sysFavorExperienceMapper.insert(params);
+			}
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -86,11 +100,17 @@ public class SysFavorController extends BaseController{
 			
 			if (hasData != null) 
 			{
+				SysExperience exParams = new SysExperience();
+				exParams.setId(id);
+				SysExperience experience = sysExperienceMapper.selectOne(new QueryWrapper<SysExperience>(exParams));
+				if (experience.getFavorNum() > 0) experience.setFavorNum(experience.getFavorNum() - 1);
+				sysExperienceMapper.update(experience, new UpdateWrapper<SysExperience>().eq("id", id));
 				Map<String, Object> columnMap = new HashMap<String, Object>();
 				
 				columnMap.put("user_id", userId);
 				columnMap.put("experience_id", id);
 				sysFavorExperienceMapper.deleteByMap(columnMap);
+				result.setMessage("已成功移除收藏夹,点击确定前往查看!");
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
